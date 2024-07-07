5393fc2f1c6929e461115ca664f6ee563c9b5b6f
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-07-07 17:31:33 -0300

    Update script.js

--- 04/src/script.js
+++ 04/src/script.js
@@ -1,59 +1,81 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group();
+group.position.y = 1
+group.rotation.y = Math.PI
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+group.add(cube3);
+
+cube2.position.x = -2
+cube3.position.x = 2
+
+
+// const mesh = new THREE.Mesh(geometry, material);
 
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+// mesh.position.set(0.7, -0.6, 1);
 
 // Scale
-mesh.scale.set(2, 0.5, 0.5)
+// mesh.scale.set(2, 0.5, 0.5);
 
 // Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, 0)
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, 0);
 
-scene.add(mesh)
+// scene.add(mesh);
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
-
-camera.lookAt(mesh.position)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
