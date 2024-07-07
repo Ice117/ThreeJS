460ca7b723f338254ab5e59e143d6d794a3476ce
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-07-07 17:51:13 -0300

    .

--- 05/src/script.js
+++ 05/src/script.js
@@ -28,4 +28,25 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Time
+// let time = Date.now()
+
+// Animation
+const tick = () => {
+    // Time
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
+
+    // Update objects
+    mesh.rotation.y += 0.001 * deltaTime
+    mesh.rotation.x += 0.001 * deltaTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
