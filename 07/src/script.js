e6bc53863f4da16acd49a73207a1c659fb95e56c
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-07-08 23:56:17 -0300

    .

--- 07/src/script.js
+++ 07/src/script.js
@@ -22,9 +22,22 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () => {
+    //update size
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
 
 /**
  * Camera
