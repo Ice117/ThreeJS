26d7a684d1f8b0312295a3107f0dda1a0608c216
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-09-01 18:16:51 -0300

    .

--- 26/src/Experience/Utils/Resources.js
+++ 26/src/Experience/Utils/Resources.js
@@ -4,6 +4,12 @@ export default class Resources extends EventEmitter{
   constructor(sources){
     super()
 
+    // Options
     this.sources = sources
+
+    // Setup
+    this.items = {}
+    this.toLoad = this.sources.length
+    this.loaded = 0
   }
 }
