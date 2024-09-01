657dd7c540e61ac08eae94a7b2bb6beba5c095d9
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-09-01 14:29:39 -0300

    .

--- 26/src/Experience/Experience.js
+++ 26/src/Experience/Experience.js
@@ -1,9 +1,18 @@
 import * as THREE from 'three'
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/time.js";
+import Camera from "./Camera.js";
+
+let instance = null
 
 export default class Experience {
   constructor(canvas) {
+    if(instance){
+      return instance
+    }
+
+    instance = this
+
     // Global access
     window.experience = this;
 
@@ -14,6 +23,7 @@ export default class Experience {
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene()
+    this.camera = new Camera()
 
     // Resize event
     this.sizes.on("resize", () => {
