855ab7513ccf0d4bfaa95bf1a124ae9c294ab4d0
Gabriel Elias Mouro <gabrieleliasmouro@gmail.com>
2024-09-01 18:27:01 -0300

    .

--- 26/src/Experience/Utils/Resources.js
+++ 26/src/Experience/Utils/Resources.js
@@ -1,15 +1,46 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import EventEmitter from "./EventEmitter";
+import * as THREE from "three";
 
-export default class Resources extends EventEmitter{
-  constructor(sources){
-    super()
+export default class Resources extends EventEmitter {
+  constructor(sources) {
+    super();
 
     // Options
-    this.sources = sources
+    this.sources = sources;
 
     // Setup
-    this.items = {}
-    this.toLoad = this.sources.length
-    this.loaded = 0
+    this.items = {};
+    this.toLoad = this.sources.length;
+    this.loaded = 0;
+
+    this.setLoaders();
+    this.startLoading();
+  }
+
+  setLoaders() {
+    this.loaders = {};
+    this.loaders.gltfLoader = new GLTFLoader();
+    this.loaders.textureLoader = new THREE.TextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+  }
+
+  startLoading() {
+    for (const source of this.sources) {
+      if (source.type === "gltfModel") {
+        this.loaders.gltfLoader.load(
+          source.path, 
+          (file) => {
+          console.log(source, file);
+        });
+      }
+      else if(source.type === 'cubeTexture'){
+        this.loaders.cubeTextureLoader.load(
+          source.path, 
+          (file) => {
+          console.log(source, file);
+        });
+      }
+    }
   }
 }
